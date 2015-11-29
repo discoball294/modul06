ca3a39922f9938f068edde849ffe9351b4388120
Bryan Kristian <discoball294@gmail.com>
2015-11-29 21:15:11 +0700

    bugfixes

--- dbConnect.php
+++ dbConnect.php
@@ -14,3 +14,5 @@ define('DB_NAME','modul06');
 $dbc=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 if(mysqli_connect_error())
     die('Tidak dapat terhubung dengan database'.mysql_error());
+else
+    echo "Koneksi berhasil";
